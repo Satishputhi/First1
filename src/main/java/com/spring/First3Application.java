b5a01bf38190b41389c8dc3c4a0ce0873c70f5ca
Satishputhi <satishpooti05@gmail.com>
2023-08-27 13:52:40 +0530

    First Commit

---
+++ src/main/java/com/spring/First3Application.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class First3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(First3Application.class, args);
+	}
+
+}
